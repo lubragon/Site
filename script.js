2450eb89a5642dba1c1d4a359de6ca2721fa3a89
Lucas Bragança Gonçalves <100163176+lubragon@users.noreply.github.com>
2022-06-08 14:07:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,104 +1,49 @@
-
-var wa = document.getElementById("grey")
-var we = document.getElementById("sala")
-
-    
 function abrirTermo() {
     $("#greyMask")[0].style["display"]= "flex";
-    $("#btConfirmar")[0].style["background-color"]= "#005BBC";
+    $("#btConfirmar")[0].style["background-color"]= "#001E3D";
 }
 
     
 function fecharTermo() {
     $("#greyMask")[0].style["display"]= "none";
-    $("#btConfirmar")[0].style["background-color"]= "white";
+    $("#btConfirmar")[0].style["background-color"]= "#005BBC";
 
 }
 
-var foc = $(".fotos")[0];
-var ve = $("#vera")[0];
-var vasofia = 	$("#btsofia")[0].style["background-color"]= "#9c0c0c";
-var vavera = $("#btvera")[0].style["background-color"]= "#9c0c0c";
+function pronto() {
+	$("#feito")[0].style["display"]= "flex";
+
+
+    $("#greyMask")[0].style["display"]= "none";
+    $("#btConfirmar")[0].style["background-color"]= "#005BBC";
+
+}
 
 function uno() {
-	if (vasofia = true) { 
-		$(".fotos")[0].setAttribute("src","1.png");
-		
-	} 
-	else if (vavera = true) {
-		$(".fotos")[0].setAttribute("src","Lucia.png");
-	}
+	$(".fotos")[0].setAttribute("src","1.png");
 
-	else {
-		$("#sofia3")[0].style["display"]= "none";
-		$("#teste3")[0].style["display"]= "none";
-	}
 	
-	
+	$("#bt1")[0].style["background-color"]= "#001E3D";
+	$("#bt2")[0].style["background-color"]= "#FFFFFF";
+	$("#bt3")[0].style["background-color"]= "#FFFFFF";
 }
-
-// f unction uno() {
-// 	if (vavera ="#9c0c0c") { 
-// 		$(".fotos")[0].setAttribute("src","1.png");
-		
-// 	} 
-// 	else if (vasofia ="#9c0c0c") {
-// 		$(".fotos")[0].setAttribute("src","Lucia.png");
-// 	}
-
-// 	else {
-// 		$("#sofia3")[0].style["display"]= "none";
-// 		$("#teste3")[0].style["display"]= "none";
-// 	}
-	
-	
-// }
-
-
-
 function dos() {
 	$(".fotos")[0].setAttribute("src","2.png");
+
+	$("#bt1")[0].style["background-color"]= "#FFFFFF";
+	$("#bt2")[0].style["background-color"]= "#001E3D";
+	$("#bt3")[0].style["background-color"]= "#FFFFFF";
 }
+	
 
 function tres() {
 	$(".fotos")[0].setAttribute("src","3.png");
-}
 
-function clicsof() {
-	$("#teste1")[0].style["display"]= "none";
-	$("#teste2")[0].style["display"]= "none";
-	$("#teste3")[0].style["display"]= "none";
+	$("#bt1")[0].style["background-color"]= "#FFFFFF";
+	$("#bt2")[0].style["background-color"]= "#FFFFFF";
+	$("#bt3")[0].style["background-color"]= "#001E3D";
 
-	$("#sofia1")[0].style["display"]= "grid";
-	$("#sofia2")[0].style["display"]= "grid";
-	$("#sofia3")[0].style["display"]= "grid";
-
-	$("#btsofia")[0].style["background-color"]= "#9c0c0c";
-	$("#btvera")[0].style["background-color"]= "white";
-}
-
-function clicvera() {
-	$("#teste1")[0].style["display"]= "grid";
-	$("#teste2")[0].style["display"]= "grid";
-	$("#teste3")[0].style["display"]= "grid";
-
-	$("#sofia1")[0].style["display"]= "none";
-	$("#sofia2")[0].style["display"]= "none";
-	$("#sofia3")[0].style["display"]= "none";
-
-	$("#btsofia")[0].style["background-color"]= "white";
-	$("#btvera")[0].style["background-color"]= "#9c0c0c";
 }
 
 
-
-
-// $("#vera2")[0].style["display"]= "";
-// $("#vera3")[0].style["display"]= "";
-
-// $("#sofia1")[0].style["display"]= "none";
-// $("#sofia2")[0].style["display"]= "none";
-// $("#sofia3")[0].style["display"]= "none";
-
-// $("#dom")[0].style["background-color"]= "#005BBC";
 
