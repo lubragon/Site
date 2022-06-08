053c6e5d42a0ea9052b4904299addc51c536d527
Lucas Bragança Gonçalves <100163176+lubragon@users.noreply.github.com>
2022-06-08 02:56:10 -0300

    Problemas com botoes

--- script.js
+++ script.js
@@ -15,66 +15,90 @@ function fecharTermo() {
 
 }
 
+var foc = $(".fotos")[0];
+var ve = $("#vera")[0];
+var vasofia = 	$("#btsofia")[0].style["background-color"]= "#9c0c0c";
+var vavera = $("#btvera")[0].style["background-color"]= "#9c0c0c";
 
+function uno() {
+	if (vasofia = true) { 
+		$(".fotos")[0].setAttribute("src","1.png");
+		
+	} 
+	else if (vavera = true) {
+		$(".fotos")[0].setAttribute("src","Lucia.png");
+	}
 
-var slider_img = $(".slider");
-var images = ['1.png', '2.png', '3.png'];
-var i = 0;
-
-
-function fotoNext() {
-    $(".fotos")[0].attr["src"]= "2.png";		 
-}
-
-
-function fotoPrev() {
-    $(".fotos")[0].attr("src","2.png");
-
-}
-
-
-
-function setImg(){
-	return slider_img.setAttribute('src', "images/" + images[i]);
+	else {
+		$("#sofia3")[0].style["display"]= "none";
+		$("#teste3")[0].style["display"]= "none";
+	}
+	
 	
 }
 
+// f unction uno() {
+// 	if (vavera ="#9c0c0c") { 
+// 		$(".fotos")[0].setAttribute("src","1.png");
+		
+// 	} 
+// 	else if (vasofia ="#9c0c0c") {
+// 		$(".fotos")[0].setAttribute("src","Lucia.png");
+// 	}
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-document.querySelector("#sala")
-
-var slider_img = $(".slider");
-var images = ['1.png', '2.png', '3.png'];
-var i = 0;
-
-
-function fotoNext(){
-	if (i >= images.length-1) i = -1;
-	i++;
-	return setImg();			 
-}
-
-
-function fotoPrev() {
-	if (i <= 0) i = images.length;	
-	i--;
-	return setImg();			 
-} 
-
-
-function setImg(){
-	return slider_img.setAttribute('src', "images/" + images[i]);
+// 	else {
+// 		$("#sofia3")[0].style["display"]= "none";
+// 		$("#teste3")[0].style["display"]= "none";
+// 	}
 	
+	
+// }
+
+
+
+function dos() {
+	$(".fotos")[0].setAttribute("src","2.png");
 }
-*\
+
+function tres() {
+	$(".fotos")[0].setAttribute("src","3.png");
+}
+
+function clicsof() {
+	$("#teste1")[0].style["display"]= "none";
+	$("#teste2")[0].style["display"]= "none";
+	$("#teste3")[0].style["display"]= "none";
+
+	$("#sofia1")[0].style["display"]= "grid";
+	$("#sofia2")[0].style["display"]= "grid";
+	$("#sofia3")[0].style["display"]= "grid";
+
+	$("#btsofia")[0].style["background-color"]= "#9c0c0c";
+	$("#btvera")[0].style["background-color"]= "white";
+}
+
+function clicvera() {
+	$("#teste1")[0].style["display"]= "grid";
+	$("#teste2")[0].style["display"]= "grid";
+	$("#teste3")[0].style["display"]= "grid";
+
+	$("#sofia1")[0].style["display"]= "none";
+	$("#sofia2")[0].style["display"]= "none";
+	$("#sofia3")[0].style["display"]= "none";
+
+	$("#btsofia")[0].style["background-color"]= "white";
+	$("#btvera")[0].style["background-color"]= "#9c0c0c";
+}
+
+
+
+
+// $("#vera2")[0].style["display"]= "";
+// $("#vera3")[0].style["display"]= "";
+
+// $("#sofia1")[0].style["display"]= "none";
+// $("#sofia2")[0].style["display"]= "none";
+// $("#sofia3")[0].style["display"]= "none";
+
+// $("#dom")[0].style["background-color"]= "#005BBC";
+
